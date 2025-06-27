8d8266f71976e683d9891ce511c163641ac3029e
FehPereira <fehpereira237@gmail.com>
2025-06-27 11:04:28 -0300

    build

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,12 +1,11 @@
 import { UserController } from '@/controllers/user-controller';
-import { errorHandling } from '@/middlewares/error-handling';
 import { Router } from 'express';
 
 const userRoutes = Router();
 const userController = new UserController();
 
-userRoutes.post('/', userController.create);
+userRoutes.post('/', userController.create.bind(userController));
 
-userRoutes.post('/session', userController.login);
+userRoutes.post('/session', userController.login.bind(userController));
 
 export { userRoutes };
